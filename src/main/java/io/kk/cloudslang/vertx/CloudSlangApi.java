19d0d8cb978d55d1c024d8d38fb9c3ab1480273f
keke <keke@KMP.local>
2016-09-22 00:24:37 +0800

    add verticle

--- src/main/java/io/kk/cloudslang/vertx/CloudSlangApi.java
+++ src/main/java/io/kk/cloudslang/vertx/CloudSlangApi.java
@@ -2,11 +2,16 @@ package io.kk.cloudslang.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ *
  * @author keke
  */
-public class CloudSlangService extends AbstractVerticle {
+public class CloudSlangApi extends AbstractVerticle {
+  private static final Logger LOG = LoggerFactory.getLogger(CloudSlangApi.class);
+
   @Override
   public void start(Future<Void> startFuture) throws Exception {
     super.start(startFuture);
